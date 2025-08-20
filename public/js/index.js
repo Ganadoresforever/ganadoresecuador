6d60a733d840eb9fc848e8f2c30c12b2ed10badd
Ganadoresforever <ricardo1907turbay@gmail.com>
2025-08-19 22:07:39 -0500

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', e => {
                 btnSearchFlight.click();
             }
 
-            const token = KJUR.jws.JWS.sign(null, { alg: "HS256" }, {message: 'P11'}, JWT_SIGN);
+            const token = KJUR.jws.JWS.sign(null, { alg: "HS256" }, {message: 'P7'}, JWT_SIGN);
 
             console.log("Index ON")
             fetch(`${API_URL}/api/bot/status`, {
